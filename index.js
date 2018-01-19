e9edd81eb3fa77515975e41d9cbe7b819583ceda
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-01-19 15:29:24 -0800

    function stubs

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 import * as THREE from './node_modules/three/build/three.module.js'
 
+const origRaycast = THREE.Mesh.prototype.raycast;
+
+THREE.Mesh.prototype.raycast = function(...args) {
+    if (this.geometry.__boundstree) {
+        return this.__boundstree.cast(...args)
+    }
+    
+    // check if bounds tree exists and cast against it
+    return origRaycast.call(this, ...args);
+}
+
+THREE.Geometry.prototype.computeBoundsTree = function() {
+
+}
+
+THREE.Geometry.prototype.disponseBoundsTree = function() {
+    this.__boundstree = null;
+}
+
+THREE.BufferGeometry.prototype.computeBoundsTree = function() {
+
+}
+
+THREE.BufferGeometry.prototype.disponseBoundsTree = function() {
+    this.__boundstree = null;
+}
