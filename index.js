1366c6572ab9535a6ff19da508546fce075843bc
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-01-20 00:33:06 -0800

    Move files around

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from './node_modules/three/build/three.module.js'
-import './MeshRaycastRewrite.js'
-import TriangleBoundsTree from './TriangleBoundsTree.js'
+import './lib/ThreeMeshRewrite.js'
+import TriangleBoundsTree from './lib/TriangleBoundsTree.js'
 
 THREE.Geometry.prototype.computeBoundsTree = function() {
     this.boundsTree = new TriangleBoundsTree(this);
