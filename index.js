ed182bc72a7745bf01d2e3409ad840cf9c3844f6
Brandon Evans <bevans@customorthopaedics.com>
2018-06-01 14:20:54 -0400

    Add option to get first hit only.
    Remove duplicate intersections for non-buffered geometry, too.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import MeshBVH from './lib/MeshBVH.js'
 const ray = new THREE.Ray();
 const inverseMatrix = new THREE.Matrix4();
 const origRaycast = THREE.Mesh.prototype.raycast;
+const origIntersectObject = THREE.Raycaster.prototype.intersectObject;
+const origIntersectObjects = THREE.Raycaster.prototype.intersectObjects;
 
 THREE.Mesh.prototype.raycast = function(raycaster, intersects) {
     if (this.geometry.boundsTree) {
@@ -12,13 +14,26 @@ THREE.Mesh.prototype.raycast = function(raycaster, intersects) {
         inverseMatrix.getInverse(this.matrixWorld);
         ray.copy(raycaster.ray).applyMatrix4(inverseMatrix);
 
-        let seenFaces = {};
-        this.geometry.boundsTree.raycast(this, raycaster, ray, intersects, seenFaces);
-        //const res = this.geometry.boundsTree.raycastFirst(this, raycaster, ray);
-        //if (res) intersects.push(res);
+        if (raycaster.firstHitOnly === true) {
+            const res = this.geometry.boundsTree.raycastFirst(this, raycaster, ray);
+            if (res) intersects.push(res);
+        } else {
+            let seenFaces = {};
+            this.geometry.boundsTree.raycast(this, raycaster, ray, intersects, seenFaces);
+        }
     } else {
         origRaycast.call(this, raycaster, intersects);
     }
+}
+
+THREE.Raycaster.prototype.intersectObject = function ( object, recursive, optionalTarget, firstHitOnly = false) {
+  this.firstHitOnly = firstHitOnly;
+  return origIntersectObject.call(this, object, recursive, optionalTarget);
+}
+
+THREE.Raycaster.prototype.intersectObjects = function ( objects, recursive, optionalTarget, firstHitOnly = false) {
+  this.firstHitOnly = firstHitOnly;
+  return origIntersectObjects.call(this, objects, recursive, optionalTarget);
 }
 
 THREE.Geometry.prototype.computeBoundsTree = function(strat) {
