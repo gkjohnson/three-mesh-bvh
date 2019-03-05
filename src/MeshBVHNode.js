70f0461c823e4918d69977e6a6987f595607bd3a
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-03-04 19:58:52 -0800

    move new cast functions to self calling functions

--- src/MeshBVHNode.js
+++ src/MeshBVHNode.js
@@ -76,7 +76,96 @@ class MeshBVHNode {
 
 	}
 
-	geometrycast( mesh, geometry, geometryToBvh, cachedObbPoints = null, cachedObbPlanes = null ) {
+	raycast( mesh, raycaster, ray, intersects ) {
+
+		if ( this.count ) intersectTris( mesh, mesh.geometry, raycaster, ray, this.offset, this.count, intersects );
+		else {
+
+			if ( this.left.intersectRay( ray, boxIntersection ) )
+				this.left.raycast( mesh, raycaster, ray, intersects );
+			if ( this.right.intersectRay( ray, boxIntersection ) )
+				this.right.raycast( mesh, raycaster, ray, intersects );
+
+		}
+
+	}
+
+	raycastFirst( mesh, raycaster, ray ) {
+
+		if ( this.count ) {
+
+			return intersectClosestTri( mesh, mesh.geometry, raycaster, ray, this.offset, this.count );
+
+		} else {
+
+
+			// consider the position of the split plane with respect to the oncoming ray; whichever direction
+			// the ray is coming from, look for an intersection among that side of the tree first
+			const splitAxis = this.splitAxis;
+			const xyzAxis = xyzFields[ splitAxis ];
+			const rayDir = ray.direction[ xyzAxis ];
+			const leftToRight = rayDir >= 0;
+
+			// c1 is the child to check first
+			let c1, c2;
+			if ( leftToRight ) {
+
+				c1 = this.left;
+				c2 = this.right;
+
+			} else {
+
+				c1 = this.right;
+				c2 = this.left;
+
+			}
+
+			const c1Intersection = c1.intersectRay( ray, boxIntersection );
+			const c1Result = c1Intersection ? c1.raycastFirst( mesh, raycaster, ray ) : null;
+
+			// if we got an intersection in the first node and it's closer than the second node's bounding
+			// box, we don't need to consider the second node because it couldn't possibly be a better result
+			if ( c1Result ) {
+
+				// check only along the split axis
+				const rayOrig = ray.origin[ xyzAxis ];
+				const toPoint = rayOrig - c1Result.point[ xyzAxis ];
+				const toChild1 = rayOrig - c2.boundingData[ splitAxis ];
+				const toChild2 = rayOrig - c2.boundingData[ splitAxis + 3 ];
+
+				const toPointSq = toPoint * toPoint;
+				if ( toPointSq <= toChild1 * toChild1 && toPointSq <= toChild2 * toChild2 ) {
+
+					return c1Result;
+
+				}
+
+			}
+
+			// either there was no intersection in the first node, or there could still be a closer
+			// intersection in the second, so check the second node and then take the better of the two
+			const c2Intersection = c2.intersectRay( ray, boxIntersection );
+			const c2Result = c2Intersection ? c2.raycastFirst( mesh, raycaster, ray ) : null;
+
+			if ( c1Result && c2Result ) {
+
+				return c1Result.distance <= c2Result.distance ? c1Result : c2Result;
+
+			} else {
+
+				return c1Result || c2Result || null;
+
+			}
+
+		}
+
+	}
+
+}
+
+MeshBVHNode.prototype.geometrycast = ( function() {
+
+	return function geometrycast( mesh, geometry, geometryToBvh, cachedObbPoints = null, cachedObbPlanes = null ) {
 
 		if ( cachedObbPlanes === null ) {
 
@@ -198,9 +287,13 @@ class MeshBVHNode {
 
 		}
 
-	}
+	};
 
-	boxcast( mesh, box, boxToBvh, triangleCallback = null, cachedObbPoints = null, cachedObbPlanes = null ) {
+} )();
+
+MeshBVHNode.prototype.boxcast = ( function() {
+
+	return function boxcast( mesh, box, boxToBvh, triangleCallback = null, cachedObbPoints = null, cachedObbPlanes = null ) {
 
 		if ( cachedObbPlanes === null ) {
 
@@ -259,9 +352,13 @@ class MeshBVHNode {
 
 		}
 
-	}
+	};
 
-	spherecast( mesh, sphere, triangleCallback ) {
+} )();
+
+MeshBVHNode.prototype.spherecast = ( function () {
+
+	return function spherecast( mesh, sphere, triangleCallback ) {
 
 		if ( this.count ) {
 
@@ -310,91 +407,6 @@ class MeshBVHNode {
 
 		}
 
-	}
+	};
 
-	raycast( mesh, raycaster, ray, intersects ) {
-
-		if ( this.count ) intersectTris( mesh, mesh.geometry, raycaster, ray, this.offset, this.count, intersects );
-		else {
-
-			if ( this.left.intersectRay( ray, boxIntersection ) )
-				this.left.raycast( mesh, raycaster, ray, intersects );
-			if ( this.right.intersectRay( ray, boxIntersection ) )
-				this.right.raycast( mesh, raycaster, ray, intersects );
-
-		}
-
-	}
-
-	raycastFirst( mesh, raycaster, ray ) {
-
-		if ( this.count ) {
-
-			return intersectClosestTri( mesh, mesh.geometry, raycaster, ray, this.offset, this.count );
-
-		} else {
-
-
-			// consider the position of the split plane with respect to the oncoming ray; whichever direction
-			// the ray is coming from, look for an intersection among that side of the tree first
-			const splitAxis = this.splitAxis;
-			const xyzAxis = xyzFields[ splitAxis ];
-			const rayDir = ray.direction[ xyzAxis ];
-			const leftToRight = rayDir >= 0;
-
-			// c1 is the child to check first
-			let c1, c2;
-			if ( leftToRight ) {
-
-				c1 = this.left;
-				c2 = this.right;
-
-			} else {
-
-				c1 = this.right;
-				c2 = this.left;
-
-			}
-
-			const c1Intersection = c1.intersectRay( ray, boxIntersection );
-			const c1Result = c1Intersection ? c1.raycastFirst( mesh, raycaster, ray ) : null;
-
-			// if we got an intersection in the first node and it's closer than the second node's bounding
-			// box, we don't need to consider the second node because it couldn't possibly be a better result
-			if ( c1Result ) {
-
-				// check only along the split axis
-				const rayOrig = ray.origin[ xyzAxis ];
-				const toPoint = rayOrig - c1Result.point[ xyzAxis ];
-				const toChild1 = rayOrig - c2.boundingData[ splitAxis ];
-				const toChild2 = rayOrig - c2.boundingData[ splitAxis + 3 ];
-
-				const toPointSq = toPoint * toPoint;
-				if ( toPointSq <= toChild1 * toChild1 && toPointSq <= toChild2 * toChild2 ) {
-
-					return c1Result;
-
-				}
-
-			}
-
-			// either there was no intersection in the first node, or there could still be a closer
-			// intersection in the second, so check the second node and then take the better of the two
-			const c2Intersection = c2.intersectRay( ray, boxIntersection );
-			const c2Result = c2Intersection ? c2.raycastFirst( mesh, raycaster, ray ) : null;
-
-			if ( c1Result && c2Result ) {
-
-				return c1Result.distance <= c2Result.distance ? c1Result : c2Result;
-
-			} else {
-
-				return c1Result || c2Result || null;
-
-			}
-
-		}
-
-	}
-
-}
+} )();
