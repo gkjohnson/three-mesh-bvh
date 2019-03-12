c9a72f216c5a3ad1a035b26a9f804c945a26d075
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-03-11 17:54:05 -0700

    build

--- umd/index.js
+++ umd/index.js
@@ -14,23 +14,8 @@
 	var uvB = new THREE.Vector2();
 	var uvC = new THREE.Vector2();
 
-	var barycoord = new THREE.Vector3();
 	var intersectionPoint = new THREE.Vector3();
 	var intersectionPointWorld = new THREE.Vector3();
-
-	function uvIntersection( point, p1, p2, p3, uv1, uv2, uv3 ) {
-
-		THREE.Triangle.getBarycoord( point, p1, p2, p3, barycoord );
-
-		uv1.multiplyScalar( barycoord.x );
-		uv2.multiplyScalar( barycoord.y );
-		uv3.multiplyScalar( barycoord.z );
-
-		uv1.add( uv2 ).add( uv3 );
-
-		return uv1.clone();
-
-	}
 
 	function checkIntersection( object, material, raycaster, ray, pA, pB, pC, point ) {
 
@@ -78,7 +63,7 @@
 				uvB.fromBufferAttribute( uv, b );
 				uvC.fromBufferAttribute( uv, c );
 
-				intersection.uv = uvIntersection( intersectionPoint, vA, vB, vC, uvA, uvB, uvC );
+				intersection.uv = THREE.Triangle.getUV( intersectionPoint, vA, vB, vC, uvA, uvB, uvC, new THREE.Vector2( ) );
 
 			}
 
