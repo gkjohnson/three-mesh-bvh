925355d99ed3dff016c2ef9a8512c3fdb40e724e
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-03-09 20:02:29 -0800

    Add missing file

---
+++ src/Utils/BufferNodeUtils.js
@@ -0,0 +1,11 @@
+export function arrayToBox( nodeIndex32, array, target ) {
+
+	target.min.x = array[ nodeIndex32 ];
+	target.min.y = array[ nodeIndex32 + 1 ];
+	target.min.z = array[ nodeIndex32 + 2 ];
+
+	target.max.x = array[ nodeIndex32 + 3 ];
+	target.max.y = array[ nodeIndex32 + 4 ];
+	target.max.z = array[ nodeIndex32 + 5 ];
+
+}
