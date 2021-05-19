6786066aa036f050a19953546345f114a227577f
gonnavis <gonnavis@163.com>
2021-05-19 16:03:21 +0800

    Add `esm` build output.

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,38 @@
-export default {
-	input: './src/index.js',
-	treeshake: false,
-	external: p => /^three/.test( p ),
-
-	output: {
-
-		name: 'MeshBVHLib',
-		extend: true,
-		format: 'umd',
-		file: './umd/index.js',
-		sourcemap: true,
-
-		globals: p => /^three/.test( p ) ? 'THREE' : null,
-
+export default [
+	{
+		input: './src/index.js',
+		treeshake: false,
+		external: p => /^three/.test( p ),
+	
+		output: {
+	
+			name: 'MeshBVHLib',
+			extend: true,
+			format: 'umd',
+			file: './umd/index.js',
+			sourcemap: true,
+	
+			globals: p => /^three/.test( p ) ? 'THREE' : null,
+	
+		},
+	
 	},
-
-};
+	{
+		input: './src/index.js',
+		treeshake: false,
+		external: p => /^three/.test( p ),
+	
+		output: {
+	
+			name: 'MeshBVHLib',
+			extend: true,
+			format: 'esm',
+			file: './esm/index.js',
+			sourcemap: true,
+	
+			globals: p => /^three/.test( p ) ? 'THREE' : null,
+	
+		},
+	
+	}
+];
