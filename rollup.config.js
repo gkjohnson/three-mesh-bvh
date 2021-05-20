228c272e542209ea2e299d0511908a26b49fcc94
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-05-20 13:18:57 -0700

    updates

--- rollup.config.js
+++ rollup.config.js
@@ -3,32 +3,32 @@ export default [
 		input: './src/index.js',
 		treeshake: false,
 		external: p => /^three/.test( p ),
-	
+
 		output: {
-	
+
 			name: 'MeshBVHLib',
 			extend: true,
 			format: 'umd',
 			file: './umd/index.js',
 			sourcemap: true,
-	
+
 			globals: p => /^three/.test( p ) ? 'THREE' : null,
-	
+
 		},
-	
+
 	},
 	{
 		input: './src/index.js',
 		treeshake: false,
 		external: p => /^three/.test( p ),
-	
+
 		output: {
-	
+
 			format: 'esm',
 			file: './esm/index.js',
 			sourcemap: true,
-	
+
 		},
-	
+
 	}
 ];
