524ec1726fb0ebe80e73a4a068d5fc17324dfae9
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-07-25 08:39:59 -0700

    add comments

--- src/MeshBVHVisualizer.js
+++ src/MeshBVHVisualizer.js
@@ -82,16 +82,19 @@ class MeshBVHRootVisualizer extends LineSegments {
 
 			// fill in the index buffer to point to the corner points
 			const edgeIndices = new Uint8Array( [
+				// x axis
 				0, 4,
 				1, 5,
 				2, 6,
 				3, 7,
 
+				// y axis
 				0, 2,
 				1, 3,
 				4, 6,
 				5, 7,
 
+				// z axis
 				0, 1,
 				2, 3,
 				4, 5,
