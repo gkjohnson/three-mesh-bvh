0da29a7371a60b9aebbd1eddfcf60f3d06d68643
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-08-09 22:06:20 -0700

    surface area score for benchmark

--- benchmark/run-benchmark.js
+++ benchmark/run-benchmark.js
@@ -21,8 +21,8 @@ const target2 = new THREE.Vector3();
 const geometry = new THREE.TorusBufferGeometry( 5, 5, 700, 300 );
 const mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial() );
 const raycaster = new THREE.Raycaster();
-raycaster.ray.origin.set( 10, 10, 10 );
-raycaster.ray.direction.set( - 1, - 1, - 1 );
+raycaster.ray.origin.set( 10, 20, 30 );
+raycaster.ray.direction.set( - 1, - 2, - 3 );
 
 function logExtremes( bvh, geometry ) {
 
@@ -38,7 +38,7 @@ function logExtremes( bvh, geometry ) {
 		`\t\ttris: ${extremes.tris.min}, ${extremes.tris.max}\n` +
 		`\t\tdepth: ${extremes.depth.min}, ${extremes.depth.max}\n` +
 		`\t\tsplits: ${extremes.splits[ 0 ]}, ${extremes.splits[ 1 ]}, ${extremes.splits[ 2 ]}\n` +
-		`\t\tsurfaceAreaScore: ${extremes.tsurfaceAreaScore}\n`
+		`\t\tsurfaceAreaScore: ${extremes.surfaceAreaScore.toFixed( 6 )}\n`
 	);
 
 }
