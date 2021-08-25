f672ece601629b19ec8f1f5101874128380f5878
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-08-25 11:07:08 -0700

    Update inspector.js

--- example/inspector.js
+++ example/inspector.js
@@ -457,7 +457,7 @@ function render() {
 
 	fsQuad.material.threshold = params.visualization.traversalThreshold;
 	fsQuad.material.outlineAlpha = params.visualization.outline ? 0.5 : 0.0;
-	renderer.getSize( fsQuad.material.resolution );
+	fsQuad.material.resolution.set( renderTarget.width, renderTarget.height );
 
 	// render bvh
 	benchmarkViz.visible = false;
