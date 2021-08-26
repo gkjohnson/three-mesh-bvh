7f27319cdf71aed4fc3c840edebf2ffaa2cb0a13
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-08-26 08:50:22 -0700

    fix LGTM warning

--- src/buildFunctions.js
+++ src/buildFunctions.js
@@ -508,7 +508,8 @@ export function buildTree( geo, options ) {
 			reachedMaxDepth = true;
 			if ( verbose ) {
 
-				console.warn( `MeshBVH: Max depth of ${ maxDepth } reached when generating BVH. Consider increasing maxDepth.`, geo );
+				console.warn( `MeshBVH: Max depth of ${ maxDepth } reached when generating BVH. Consider increasing maxDepth.` );
+				console.warn( geo );
 
 			}
 
