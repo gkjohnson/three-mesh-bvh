73728856ad6350864480f46f42f90f254be6031e
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-08-27 16:55:28 -0700

    debug example updates

--- example/edgeIntersect.js
+++ example/edgeIntersect.js
@@ -3,20 +3,15 @@ import * as dat from 'dat.gui';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
-import MeshBVHVisualizer from '../src/MeshBVHVisualizer.js';
 import { acceleratedRaycast, computeBoundsTree, disposeBoundsTree } from '../src/index.js';
-import "@babel/polyfill";
+import { SeparatingAxisTriangle } from '../src/Utils/SeparatingAxisTriangle.js';
+import { setTriangle } from '../src/Utils/TriangleUtils.js';
 
 THREE.Mesh.prototype.raycast = acceleratedRaycast;
 THREE.BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
 THREE.BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
 
 const params = {
-
-	speed: 1,
-	visualizeBounds: false,
-	visualBoundsDepth: 10,
-	shape: 'sphere',
 	position: new THREE.Vector3(),
 	rotation: new THREE.Euler(),
 	scale: new THREE.Vector3( 1, 1, 1 ),
@@ -24,8 +19,10 @@ const params = {
 
 let stats;
 let scene, camera, renderer, orbitControls, boundsViz, transformControls;
-let targetMesh;
-let shapes = {};
+let mesh1, mesh2, group, line;
+
+init();
+render();
 
 function init() {
 
@@ -48,16 +45,36 @@ function init() {
 	scene.add( new THREE.AmbientLight( 0xffffff, 0.4 ) );
 
 	// geometry setup
-	const radius = 1;
-	const tube = 0.4;
-	const tubularSegments = 400;
-	const radialSegments = 100;
+	// const radius = 1;
+	// const tube = 0.4;
+	// const tubularSegments = 400;
+	// const radialSegments = 100;
 
-	const knotGeometry = new THREE.TorusKnotBufferGeometry( radius, tube, tubularSegments, radialSegments );
+	// const knotGeometry = new THREE.TorusKnotBufferGeometry( radius, tube, tubularSegments, radialSegments );
 	const material = new THREE.MeshPhongMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
-	targetMesh = new THREE.Mesh( knotGeometry, material );
-	targetMesh.geometry.computeBoundsTree();
-	scene.add( targetMesh );
+	// targetMesh = new THREE.Mesh( knotGeometry, material );
+	// targetMesh.geometry.computeBoundsTree();
+	// scene.add( targetMesh );
+
+	const tri1 = new THREE.BufferGeometry();
+	tri1.setFromPoints( [
+		new THREE.Vector3( 1, 0, 0 ),
+		new THREE.Vector3( - 1, 0, 0 ),
+		new THREE.Vector3( 0, 1, 0 ),
+	] );
+	tri1.computeVertexNormals();
+	tri1.computeBoundsTree();
+
+	mesh1 = new THREE.Mesh( tri1, material );
+	mesh2 = new THREE.Mesh( tri1, material );
+	scene.add( mesh1, mesh2 );
+
+	const lineGeometry = new THREE.BufferGeometry();
+	lineGeometry.setFromPoints( [ new THREE.Vector3( 0, 1, 0 ), new THREE.Vector3( 0, - 1, 0 ) ] );
+	line = new THREE.Line( lineGeometry, new THREE.LineBasicMaterial( { color: 0xff0000, depthTest: false } ) );
+	// line.visible = false;
+	scene.add( line );
+
 
 	// camera setup
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 50 );
@@ -68,63 +85,17 @@ function init() {
 	transformControls = new TransformControls( camera, renderer.domElement );
 	scene.add( transformControls );
 
+	group = new THREE.Group();
+	transformControls.attach( group );
+	scene.add( group );
+
 	orbitControls = new OrbitControls( camera, renderer.domElement );
 
 	// stats setup
 	stats = new Stats();
 	document.body.appendChild( stats.dom );
 
-	const shapeMaterial = new THREE.MeshStandardMaterial( {
-		metalness: 0.1,
-		transparent: true,
-		opacity: 0.75,
-		premultipliedAlpha: true
-	} );
-	shapes.sphere = new THREE.Mesh( new THREE.SphereBufferGeometry( 1, 50, 50 ), shapeMaterial );
-	scene.add( shapes.sphere );
-
-	shapes.box = new THREE.Mesh( new THREE.BoxBufferGeometry( 1, 1, 1 ), shapeMaterial );
-	scene.add( shapes.box );
-
-	shapes.geometry = new THREE.Mesh( new THREE.TorusKnotBufferGeometry( .5, .2, 200, 50 ), shapeMaterial );
-	shapes.geometry.geometry.computeBoundsTree();
-	scene.add( shapes.geometry );
-
-	// Code for debugging triangle intersection
-	// const t1 = new THREE.Triangle();
-	// const t2 = new THREE.Triangle();
-
-	// t1.a.set( - 1, - 1, 0 );
-	// t1.b.set( 1, - 1, 0 );
-	// t1.c.set( 0, 1, 0 );
-
-	// t2.a.set( - 1, 0, - 0.5 );
-	// t2.b.set( 0, 0, 1 );
-	// t2.c.set( - 2, 0, 1 );
-
-	// const res = triangleIntersectsTriangle( t1, t2 );
-	// console.log( 'intersects ', res );
-
-	// var lineMat = new THREE.LineBasicMaterial({ color: 0x0000ff });
-
-	// var geometry = new THREE.Geometry();
-	// geometry.vertices.push( t1.a, t1.b, t1.c, t1.a );
-	// var line = new THREE.Line( geometry, lineMat );
-	// scene.add( line );
-
-	// var geometry = new THREE.Geometry();
-	// geometry.vertices.push( t2.a, t2.b, t2.c, t2.a );
-	// var line = new THREE.Line( geometry, lineMat );
-	// scene.add( line );
-
-	// 	targetMesh.visible = false;
-
 	const gui = new dat.GUI();
-	gui.add( params, 'speed' ).min( 0 ).max( 10 );
-	gui.add( params, 'visualizeBounds' ).onChange( () => updateFromOptions() );
-	gui.add( params, 'visualBoundsDepth' ).min( 1 ).max( 40 ).step( 1 ).onChange( () => updateFromOptions() );
-	gui.add( params, 'shape', [ 'sphere', 'box', 'geometry' ] );
-
 	gui.add( transformControls, 'mode', [ 'translate', 'rotate' ] );
 
 	const posFolder = gui.addFolder( 'position' );
@@ -143,9 +114,9 @@ function init() {
 
 	transformControls.addEventListener( 'change', function () {
 
-		params.position.copy( shapes[ params.shape ].position );
-		params.rotation.copy( shapes[ params.shape ].rotation );
-		params.scale.copy( shapes[ params.shape ].scale );
+		params.position.copy( mesh1.position );
+		params.rotation.copy( mesh1.rotation );
+		params.scale.copy( mesh1.scale );
 		gui.updateDisplay();
 
 	} );
@@ -202,100 +173,83 @@ function init() {
 
 }
 
-function updateFromOptions() {
-
-	// Update bounds viz
-	if ( boundsViz && ! params.visualizeBounds ) {
-
-		scene.remove( boundsViz );
-		boundsViz = null;
-
-	}
-
-	if ( ! boundsViz && params.visualizeBounds ) {
-
-		boundsViz = new MeshBVHVisualizer( targetMesh );
-		scene.add( boundsViz );
-
-	}
-
-	if ( boundsViz ) {
-
-		boundsViz.depth = params.visualBoundsDepth;
-
-	}
-
-}
-
 let lastTime = window.performance.now();
 function render() {
+
+	requestAnimationFrame( render );
 
 	const delta = window.performance.now() - lastTime;
 	lastTime = window.performance.now();
 
-	targetMesh.rotation.y += params.speed * delta * 0.001;
-	targetMesh.updateMatrixWorld();
+	// targetMesh.rotation.y += params.speed * delta * 0.001;
+	// targetMesh.updateMatrixWorld();
 
-	stats.begin();
+	mesh2.position.copy( group.position );
+	mesh2.rotation.copy( group.rotation );
+	mesh2.scale.copy( group.scale );
+	mesh2.updateMatrixWorld();
+
+	const matrix = new THREE.Matrix4()
+		.copy( mesh1.matrixWorld )
+		.invert()
+		.multiply( mesh2.matrixWorld );
+
+	const results = [];
+	mesh1.geometry.boundsTree.shapecast( {
+
+		intersectsBounds: () => true,
+
+		intersectsTriangle: tri => {
+
+			mesh2.geometry.boundsTree.shapecast( {
+
+				intersectsRange: ( offset, count ) => {
+
+					const edge = new THREE.Line3();
+					const tri2 = new SeparatingAxisTriangle();
+					setTriangle( tri2, offset * 3, mesh2.geometry.index, mesh2.geometry.attributes.position );
+					tri2.a.applyMatrix4( matrix );
+					tri2.b.applyMatrix4( matrix );
+					tri2.c.applyMatrix4( matrix );
+
+					tri2.needsUpdate = true;
+
+					if ( tri.intersectsTriangle( tri2, edge ) ) {
+
+						console.log( 'INTERSECTED');
+						results.push( edge.start, edge.end );
+
+					}
+
+				},
+
+			} );
+
+		},
+
+	} );
+
+	if ( results.length ) {
+
+		// console.log( results );
+		line.geometry.dispose();
+		line.geometry.setFromPoints( results );
+		line.visible = true;
+
+	} else {
+
+		line.visible = false;
+
+	}
+
 
 	if ( boundsViz ) boundsViz.update();
 
 	renderer.render( scene, camera );
+
+	stats.begin();
 	stats.end();
 
-	// casts
-	for ( const shape in shapes ) shapes[ shape ].visible = false;
-
-	const s = params.shape;
-	const shape = shapes[ s ];
-	shape.visible = true;
-	shape.position.copy( params.position );
-	shape.rotation.copy( params.rotation );
-	shape.scale.copy( params.scale );
-
-	const transformMatrix =
-		new THREE.Matrix4()
-		    .copy( targetMesh.matrixWorld ).invert()
-			.multiply( shape.matrixWorld );
-
-	if ( s === 'sphere' ) {
-
-		const sphere = new THREE.Sphere( undefined, 1 );
-		sphere.applyMatrix4( transformMatrix );
-
-		const hit = targetMesh.geometry.boundsTree.intersectsSphere( sphere );
-		shape.material.color.set( hit ? 0xE91E63 : 0x666666 );
-		shape.material.emissive.set( 0xE91E63 ).multiplyScalar( hit ? 0.25 : 0 );
-
-	} else if ( s === 'box' ) {
-
-		const box = new THREE.Box3();
-		box.min.set( - 0.5, - 0.5, - 0.5 );
-		box.max.set( 0.5, 0.5, 0.5 );
-
-		const hit = targetMesh.geometry.boundsTree.intersectsBox( box, transformMatrix );
-		shape.material.color.set( hit ? 0xE91E63 : 0x666666 );
-		shape.material.emissive.set( 0xE91E63 ).multiplyScalar( hit ? 0.25 : 0 );
-
-	} else if ( s === 'geometry' ) {
-
-		const hit = targetMesh.geometry.boundsTree.intersectsGeometry( shape.geometry, transformMatrix );
-		shape.material.color.set( hit ? 0xE91E63 : 0x666666 );
-		shape.material.emissive.set( 0xE91E63 ).multiplyScalar( hit ? 0.25 : 0 );
-
-	}
-
-	if ( transformControls.object !== shape ) {
-
-		transformControls.attach( shape );
-
-	}
-
-	requestAnimationFrame( render );
 
 }
 
-
-init();
-updateFromOptions();
-render();
