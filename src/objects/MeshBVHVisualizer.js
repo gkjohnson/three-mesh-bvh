d3a949668bb343663d20d6257487c9985f410b48
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-08-29 09:52:11 -0700

    add /* @__PURE__ */ identifiers

--- src/objects/MeshBVHVisualizer.js
+++ src/objects/MeshBVHVisualizer.js
@@ -1,7 +1,7 @@
 import { LineBasicMaterial, BufferAttribute, Box3, Group, MeshBasicMaterial, Object3D, BufferGeometry } from 'three';
 import { arrayToBox } from '../utils/ArrayBoxUtilities.js';
 
-const boundingBox = new Box3();
+const boundingBox = /* @__PURE__ */ new Box3();
 class MeshBVHRootVisualizer extends Object3D {
 
 	get isMesh() {
