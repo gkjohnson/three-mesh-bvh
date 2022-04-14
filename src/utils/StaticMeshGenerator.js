17f259eee4ecd56b4114fe24d671c18032f7c075
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-04-14 12:33:07 -0700

    improve static mesh generation

--- src/utils/StaticMeshGenerator.js
+++ src/utils/StaticMeshGenerator.js
@@ -1,7 +1,27 @@
-import { BufferAttribute, BufferGeometry } from 'three';
+import { BufferAttribute, BufferGeometry, Vector3, Vector4, Matrix4, Matrix3 } from 'three';
 import { mergeBufferGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 
+const _vector4 = /*@__PURE__*/ new Vector4();
+const _vector = /*@__PURE__*/ new Vector3();
+const _positionVector = /*@__PURE__*/ new Vector3();
+const _normalVector = /*@__PURE__*/ new Vector3();
+const _tangentVector = /*@__PURE__*/ new Vector3();
+
+const _morphVector = /*@__PURE__*/ new Vector3();
+const _temp = /*@__PURE__*/ new Vector3();
+
+const _skinIndex = /*@__PURE__*/ new Vector4();
+const _skinWeight = /*@__PURE__*/ new Vector4();
+const _matrix = /*@__PURE__*/ new Matrix4();
+const _boneMatrix = /*@__PURE__*/ new Matrix4();
+
 function validateAttributes( attr1, attr2 ) {
+
+	if ( ! attr1 && ! attr2 ) {
+
+		return;
+
+	}
 
 	const sameCount = attr1.count === attr2.count;
 	const sameNormalized = attr1.normalized === attr2.normalized;
@@ -56,7 +76,82 @@ function copyAttributeContents( attr, target, targetOffset = 0 ) {
 
 }
 
-export class StaticMeshGenerator {
+function addScaledMatrix( target, matrix, scale ) {
+
+	const targetArray = target.elements;
+	const matrixArray = matrix.elements;
+	for ( let i = 0, l = matrixArray.length; i < l; i ++ ) {
+
+		targetArray[ i ] += matrixArray[ i ] * scale;
+
+	}
+
+}
+
+function boneNormalTransform( mesh, index, target ) {
+
+	const skeleton = mesh.skeleton;
+	const geometry = mesh.geometry;
+
+	_skinIndex.fromBufferAttribute( geometry.attributes.skinIndex, index );
+	_skinWeight.fromBufferAttribute( geometry.attributes.skinWeight, index );
+
+	_matrix.elements.fill( 0 );
+
+	for ( let i = 0; i < 4; i ++ ) {
+
+		const weight = _skinWeight.getComponent( i );
+
+		if ( weight !== 0 ) {
+
+			const boneIndex = _skinIndex.getComponent( i );
+			_boneMatrix.multiplyMatrices( skeleton.bones[ boneIndex ].matrixWorld, skeleton.boneInverses[ boneIndex ] );
+
+			addScaledMatrix( _matrix, _boneMatrix, weight );
+
+		}
+
+	}
+
+	// debugger;
+
+	_matrix.multiply( mesh.bindMatrix ).premultiply( mesh.bindMatrixInverse );
+	_vector4.set( target.x, target.y, target.z, 0.0 ).applyMatrix4( _matrix );
+	target.set( _vector4.x, _vector4.y, _vector4.z );
+
+	return target;
+
+}
+
+function applyMorphTarget( morphData, morphInfluences, morphTargetsRelative, i, target ) {
+
+	_morphVector.set( 0, 0, 0 );
+	for ( let j = 0, jl = morphData.length; j < jl; j ++ ) {
+
+		const influence = morphInfluences[ j ];
+		const morphAttribute = morphData[ j ];
+
+		if ( influence === 0 ) continue;
+
+		_temp.fromBufferAttribute( morphAttribute, i );
+
+		if ( morphTargetsRelative ) {
+
+			_morphVector.addScaledVector( _temp, influence );
+
+		} else {
+
+			_morphVector.addScaledVector( _temp.sub( _vector ), influence );
+
+		}
+
+	}
+
+	target.add( _morphVector );
+
+}
+
+export class StaticGeometryGenerator {
 
 	constructor( meshes ) {
 
@@ -66,10 +161,26 @@ export class StaticMeshGenerator {
 
 		}
 
-		this.meshes = meshes;
+		const finalMeshes = [];
+		meshes.forEach( object => {
+
+			object.traverse( c => {
+
+				if ( c.isMesh ) {
+
+					finalMeshes.push( c );
+
+				}
+
+			} );
+
+		} );
+
+		this.meshes = finalMeshes;
 		this.retainGroups = false;
 		this.applyWorldTransforms = true;
-		this._intermediateGeometry = new Array( meshes.length ).fill( new BufferGeometry() );
+		this.attributes = [ 'position', 'normal', 'tangent', 'uv', 'uv2' ];
+		this._intermediateGeometry = new Array( finalMeshes.length ).fill().map( () => new BufferGeometry() );
 
 	}
 
@@ -85,46 +196,198 @@ export class StaticMeshGenerator {
 		}
 
 		// TODO: change merge buffer geometries so it can be applied to an existing geometry
-		targetGeometry = mergeBufferGeometries( _intermediateGeometry, retainGroups, targetGeometry );
+		const newGeometry = mergeBufferGeometries( _intermediateGeometry, retainGroups, targetGeometry );
+		const prevIndex = targetGeometry.index;
+		targetGeometry.copy( newGeometry );
+		if ( prevIndex ) {
+
+			targetGeometry.index = prevIndex;
+
+		}
+
 		targetGeometry.needsUpdate = true;
 
-		return targetGeometry;
+		const materials = meshes.map( m => m.material );
+		return { geometry: targetGeometry, materials };
 
 	}
 
 	_convertToStaticGeometry( mesh, targetGeometry = new BufferGeometry() ) {
 
 		const geometry = mesh.geometry;
+		const applyWorldTransforms = this.applyWorldTransforms;
+		const includeNormal = this.attributes.includes( 'normal' );
+		const includeTangent = this.attributes.includes( 'tangent' );
+		const attributes = geometry.attributes;
+		const targetAttributes = targetGeometry.attributes;
+
+		// initialize the attributes if they don't exist
 		if ( ! targetGeometry.index ) {
 
 			targetGeometry.index = geometry.index;
 
 		}
 
-		if ( ! targetGeometry.attributes.position ) {
+		if ( ! targetAttributes.position ) {
 
-			targetGeometry.setAttribute( 'position', createAttributeClone( geometry.attributes.position ) );
-
-		}
-
-		if ( ! targetGeometry.attributes.normal && geometry.attributes.normal ) {
-
-			targetGeometry.setAttribute( 'normal', createAttributeClone( geometry.attributes.normal ) );
+			targetGeometry.setAttribute( 'position', createAttributeClone( attributes.position ) );
 
 		}
 
-		if ( ! targetGeometry.attributes.tangent && geometry.attributes.tangent ) {
+		if ( includeNormal && ! targetAttributes.normal && attributes.normal ) {
 
-			targetGeometry.setAttribute( 'tangent', createAttributeClone( geometry.attributes.tangent ) );
+			targetGeometry.setAttribute( 'normal', createAttributeClone( attributes.normal ) );
 
 		}
 
+		if ( includeTangent && ! targetAttributes.tangent && attributes.tangent ) {
+
+			targetGeometry.setAttribute( 'tangent', createAttributeClone( attributes.tangent ) );
+
+		}
+
+		// ensure the attributes are consistent
 		validateAttributes( geometry.index, targetGeometry.index );
-		validateAttributes( geometry.attributes.position, targetGeometry.attributes.position );
-		validateAttributes( geometry.attributes.normal, targetGeometry.attributes.normal );
-		validateAttributes( geometry.attributes.tangent, targetGeometry.attributes.tangent );
+		validateAttributes( attributes.position, targetAttributes.position );
 
-		// TODO: fill with baked geometry
+		if ( includeNormal ) {
+
+			validateAttributes( attributes.normal, targetAttributes.normal );
+
+		}
+
+		if ( includeTangent ) {
+
+			validateAttributes( attributes.tangent, targetAttributes.tangent );
+
+		}
+
+		// generate transformed vertex attribute data
+		const position = attributes.position;
+		const normal = includeNormal ? attributes.normal : null;
+		const tangent = includeTangent ? attributes.tangent : null;
+		const morphPosition = geometry.morphAttributes.position;
+		const morphNormal = geometry.morphAttributes.normal;
+		const morphTangent = geometry.morphAttributes.tangent;
+		const morphTargetsRelative = geometry.morphTargetsRelative;
+		const morphInfluences = mesh.morphTargetInfluences;
+		const normalMatrix = new Matrix3();
+		normalMatrix.getNormalMatrix( mesh.matrixWorld );
+
+		for ( let i = 0, l = attributes.position.count; i < l; i ++ ) {
+
+			_positionVector.fromBufferAttribute( position, i );
+			if ( normal ) {
+
+				_normalVector.fromBufferAttribute( normal, i );
+
+			}
+
+			if ( tangent ) {
+
+				_tangentVector.fromBufferAttribute( tangent, i );
+
+			}
+
+			// apply morph target transform
+			if ( morphInfluences ) {
+
+				if ( morphPosition ) {
+
+					applyMorphTarget( morphPosition, morphInfluences, morphTargetsRelative, i, _positionVector );
+
+				}
+
+				// TODO: this seems to be particularly slow when normals and tangents are included in the transforms
+				if ( morphNormal ) {
+
+					applyMorphTarget( morphNormal, morphInfluences, morphTargetsRelative, i, _normalVector );
+
+				}
+
+				if ( morphTangent ) {
+
+					applyMorphTarget( morphTangent, morphInfluences, morphTargetsRelative, i, _tangentVector );
+
+				}
+
+			}
+
+			// apply bone transform
+			if ( mesh.isSkinnedMesh ) {
+
+				mesh.boneTransform( i, _positionVector );
+				if ( normal ) {
+
+					boneNormalTransform( mesh, i, _normalVector );
+
+				}
+
+				if ( tangent ) {
+
+					boneNormalTransform( mesh, i, _tangentVector );
+
+				}
+
+			}
+
+			// update the vectors of the attributes
+			if ( applyWorldTransforms ) {
+
+				_positionVector.applyMatrix4( mesh.matrixWorld );
+
+			}
+
+			targetAttributes.position.setXYZ( i, _positionVector.x, _positionVector.y, _positionVector.z );
+
+			if ( normal ) {
+
+				if ( applyWorldTransforms ) {
+
+					_normalVector.applyNormalMatrix( normalMatrix );
+
+				}
+
+				targetAttributes.normal.setXYZ( i, _normalVector.x, _normalVector.y, _normalVector.z );
+
+			}
+
+			if ( tangent ) {
+
+				if ( applyWorldTransforms ) {
+
+					_tangentVector.applyNormalMatrix( normalMatrix );
+
+				}
+
+				targetAttributes.tangent.setXYZ( i, _tangentVector.x, _tangentVector.y, _tangentVector.z );
+
+			}
+
+		}
+
+		// copy other attributes over
+		for ( const i in this.attributes ) {
+
+			const key = this.attributes[ i ];
+			if ( key === 'position' || key === 'tangent' || key === 'normal' ) {
+
+				continue;
+
+			}
+
+			if ( ! targetAttributes[ key ] ) {
+
+				targetGeometry.setAttribute( key, createAttributeClone( attributes[ key ] ) );
+
+			}
+
+			validateAttributes( attributes[ key ], targetAttributes[ key ] );
+			copyAttributeContents( attributes[ key ], targetAttributes[ key ] );
+
+		}
+
+		return targetGeometry;
 
 	}
 
