1675cd81be99a7d41a3669ccd9bdafdda55b6516
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-05-19 20:21:37 -0700

    Transfer roots

--- src/workers/generateAsync.worker.js
+++ src/workers/generateAsync.worker.js
@@ -65,7 +65,7 @@ onmessage = function ( { data } ) {
 			position,
 			progress: 1,
 
-		}, [ serialized.index.buffer, position.buffer ] );
+		}, [ serialized.index.buffer, position.buffer, ...serialized.roots ] );
 
 	} catch ( error ) {
 
