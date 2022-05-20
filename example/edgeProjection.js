17465d59d18116309d2241050bbf2698a5197f8c
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-05-20 10:28:33 -0700

    comments

--- example/edgeProjection.js
+++ example/edgeProjection.js
@@ -70,6 +70,28 @@ function updateEdges() {
 
 	const edges = generateEdges( model.geometry, new THREE.Vector3( 0, 1, 0 ), 90 );
 
+	// TODO: iterate over all edges and check visibility upwards using BVH
+	for ( let i = 0, l = edges.length; i < l; i ++ ) {
+
+		const line = edges[ i ];
+		// bvh.shapecast( {
+
+			// TODO: check if the box bounds are above the lowest line point
+
+			// TODO:
+			// - track the parts of the line that are covered in an array
+			// - skip the triangle if it is completely below the line
+			// - check if the line intersects the triangle
+			//   - if it intersects at a point then shorten the edge to check to the edge the falls below the line
+			// - construct a coplanar line and triangle (set y to a common value) and check for overlap
+			// - add the overlap to an array
+
+		// } );
+
+		// construct a final set of lines by sorting & merging the overlap lines and taking only the bits that don't overlap
+
+	}
+
 	const edgeArray = [];
 	edges.forEach( l => {
 
@@ -77,6 +99,7 @@ function updateEdges() {
 		edgeArray.push( l.end.x, - 2, l.end.z );
 
 	} );
+
 	const edgeGeom = new THREE.BufferGeometry();
 	const edgeBuffer = new THREE.BufferAttribute( new Float32Array( edgeArray ), 3, true );
 	edgeGeom.setAttribute( 'position', edgeBuffer );
