43a760c5feb17a0e7a53e0aa0c42d48f0570f980
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-05-23 12:49:45 -0700

    create local variables for functions

--- example/utils/edgeUtils.js
+++ example/utils/edgeUtils.js
@@ -1,281 +1,290 @@
-import { Vector3, Triangle, Line3, MathUtils, Ray, Plane, BufferGeometry, BufferAttribute, LineSegments, LineBasicMaterial } from 'three';
+import { Vector3, Triangle, Line3, MathUtils, Plane, BufferGeometry, BufferAttribute } from 'three';
 import { ExtendedTriangle } from '../..';
 
-const _v0 = new Vector3();
-const _v1 = new Vector3();
-const _dir0 = new Vector3();
-const _dir1 = new Vector3();
-const _tempDir = new Vector3();
-const _normal = new Vector3();
-const _triangle = new Triangle();
-const _orthoPlane = new Plane();
-const _line0 = new Line3();
-const _line1 = new Line3();
-const _tempLine = new Line3();
 const _upVector = new Vector3( 0, 1, 0 );
-const _tri = new ExtendedTriangle();
 const EPSILON = 1e-16;
 
 // Modified version of js EdgesGeometry logic to handle silhouette edges
-export function generateEdges( geometry, projectionDir, thresholdAngle = 1 ) {
+export const generateEdges = ( function () {
 
-	const edges = [];
+	const _v0 = new Vector3();
+	const _v1 = new Vector3();
+	const _normal = new Vector3();
+	const _triangle = new Triangle();
 
-	const precisionPoints = 4;
-	const precision = Math.pow( 10, precisionPoints );
-	const thresholdDot = Math.cos( MathUtils.DEG2RAD * thresholdAngle );
+	return function generateEdges( geometry, projectionDir, thresholdAngle = 1 ) {
 
-	const indexAttr = geometry.getIndex();
-	const positionAttr = geometry.getAttribute( 'position' );
-	const indexCount = indexAttr ? indexAttr.count : positionAttr.count;
+		const edges = [];
 
-	const indexArr = [ 0, 0, 0 ];
-	const vertKeys = [ 'a', 'b', 'c' ];
-	const hashes = new Array( 3 );
+		const precisionPoints = 4;
+		const precision = Math.pow( 10, precisionPoints );
+		const thresholdDot = Math.cos( MathUtils.DEG2RAD * thresholdAngle );
 
-	const edgeData = {};
-	for ( let i = 0; i < indexCount; i += 3 ) {
+		const indexAttr = geometry.getIndex();
+		const positionAttr = geometry.getAttribute( 'position' );
+		const indexCount = indexAttr ? indexAttr.count : positionAttr.count;
 
-		if ( indexAttr ) {
+		const indexArr = [ 0, 0, 0 ];
+		const vertKeys = [ 'a', 'b', 'c' ];
+		const hashes = new Array( 3 );
 
-			indexArr[ 0 ] = indexAttr.getX( i );
-			indexArr[ 1 ] = indexAttr.getX( i + 1 );
-			indexArr[ 2 ] = indexAttr.getX( i + 2 );
+		const edgeData = {};
+		for ( let i = 0; i < indexCount; i += 3 ) {
 
-		} else {
+			if ( indexAttr ) {
 
-			indexArr[ 0 ] = i;
-			indexArr[ 1 ] = i + 1;
-			indexArr[ 2 ] = i + 2;
-
-		}
-
-		const { a, b, c } = _triangle;
-		a.fromBufferAttribute( positionAttr, indexArr[ 0 ] );
-		b.fromBufferAttribute( positionAttr, indexArr[ 1 ] );
-		c.fromBufferAttribute( positionAttr, indexArr[ 2 ] );
-		_triangle.getNormal( _normal );
-
-		// create hashes for the edge from the vertices
-		hashes[ 0 ] = `${ Math.round( a.x * precision ) },${ Math.round( a.y * precision ) },${ Math.round( a.z * precision ) }`;
-		hashes[ 1 ] = `${ Math.round( b.x * precision ) },${ Math.round( b.y * precision ) },${ Math.round( b.z * precision ) }`;
-		hashes[ 2 ] = `${ Math.round( c.x * precision ) },${ Math.round( c.y * precision ) },${ Math.round( c.z * precision ) }`;
-
-		// skip degenerate triangles
-		if ( hashes[ 0 ] === hashes[ 1 ] || hashes[ 1 ] === hashes[ 2 ] || hashes[ 2 ] === hashes[ 0 ] ) {
-
-			continue;
-
-		}
-
-		// iterate over every edge
-		for ( let j = 0; j < 3; j ++ ) {
-
-			// get the first and next vertex making up the edge
-			const jNext = ( j + 1 ) % 3;
-			const vecHash0 = hashes[ j ];
-			const vecHash1 = hashes[ jNext ];
-			const v0 = _triangle[ vertKeys[ j ] ];
-			const v1 = _triangle[ vertKeys[ jNext ] ];
-
-			const hash = `${ vecHash0 }_${ vecHash1 }`;
-			const reverseHash = `${ vecHash1 }_${ vecHash0 }`;
-
-			if ( reverseHash in edgeData && edgeData[ reverseHash ] ) {
-
-				// if we found a sibling edge add it into the vertex array if
-				// it meets the angle threshold and delete the edge from the map.
-				const otherNormal = edgeData[ reverseHash ].normal;
-				const meetsThreshold = _normal.dot( otherNormal ) <= thresholdDot;
-				const projectionThreshold = Math.sign( projectionDir.dot( _normal ) ) !== Math.sign( projectionDir.dot( otherNormal ) );
-				if ( meetsThreshold || projectionThreshold ) {
-
-					const line = new Line3();
-					line.start.copy( v0 );
-					line.end.copy( v1 );
-					edges.push( line );
-
-				}
-
-				edgeData[ reverseHash ] = null;
-
-			} else if ( ! ( hash in edgeData ) ) {
-
-				// if we've already got an edge here then skip adding a new one
-				edgeData[ hash ] = {
-
-					index0: indexArr[ j ],
-					index1: indexArr[ jNext ],
-					normal: _normal.clone(),
-
-				};
-
-			}
-
-		}
-
-	}
-
-	// iterate over all remaining, unmatched edges and add them to the vertex array
-	for ( const key in edgeData ) {
-
-		if ( edgeData[ key ] ) {
-
-			const { index0, index1 } = edgeData[ key ];
-			_v0.fromBufferAttribute( positionAttr, index0 );
-			_v1.fromBufferAttribute( positionAttr, index1 );
-
-			const line = new Line3();
-			line.start.copy( _v0 );
-			line.end.copy( _v1 );
-			edges.push( line );
-
-		}
-
-	}
-
-	return edges;
-
-}
-
-// outputs the overlapping segment of a coplanar line and triangle
-export function getOverlappingLine( line, triangle, lineTarget = new Line3() ) {
-
-	if ( triangle.needsUpdate ) {
-
-		triangle.needsUpdate();
-
-	}
-
-	// if the triangle is degenerate then return no overlap
-	if ( triangle.getArea() <= EPSILON ) {
-
-		return null;
-
-	}
-
-	const { points, plane } = triangle;
-
-	_line0.copy( line );
-	_line0.delta( _dir0 );
-
-	// if the line and triangle are not coplanar then return no overlap
-	const areCoplanar = plane.normal.dot( _dir0 ) === 0.0;
-	if ( ! areCoplanar ) {
-
-		return null;
-
-	}
-
-	// a plane that's orthogonal to the triangle that the line lies on
-	_dir0.cross( plane.normal ).normalize();
-	_orthoPlane.setFromNormalAndCoplanarPoint( _dir0, _line0.start );
-
-	// find the line of intersection of the triangle along the plane if it exists
-	let intersectCount = 0;
-	for ( let i = 0; i < 3; i ++ ) {
-
-		const p1 = points[ i ];
-		const p2 = points[ ( i + 1 ) % 3 ];
-
-		_tempLine.start.copy( p1 );
-		_tempLine.end.copy( p2 );
-		if ( _orthoPlane.distanceToPoint( _tempLine.end ) === 0 && _orthoPlane.distanceToPoint( _tempLine.start ) === 0 ) {
-
-			// if the edge lies on the plane then take the line
-			_line1.copy( _tempLine );
-			intersectCount = 2;
-			break;
-
-		} else if ( _orthoPlane.intersectLine( _tempLine, intersectCount === 0 ? _line1.start : _line1.end ) ) {
-
-			let p;
-			if ( intersectCount === 0 ) {
-
-				p = _line1.start;
+				indexArr[ 0 ] = indexAttr.getX( i );
+				indexArr[ 1 ] = indexAttr.getX( i + 1 );
+				indexArr[ 2 ] = indexAttr.getX( i + 2 );
 
 			} else {
 
-				p = _line1.end;
+				indexArr[ 0 ] = i;
+				indexArr[ 1 ] = i + 1;
+				indexArr[ 2 ] = i + 2;
 
 			}
 
-			if ( p.distanceTo( p2 ) === 0.0 ) {
+			const { a, b, c } = _triangle;
+			a.fromBufferAttribute( positionAttr, indexArr[ 0 ] );
+			b.fromBufferAttribute( positionAttr, indexArr[ 1 ] );
+			c.fromBufferAttribute( positionAttr, indexArr[ 2 ] );
+			_triangle.getNormal( _normal );
+
+			// create hashes for the edge from the vertices
+			hashes[ 0 ] = `${ Math.round( a.x * precision ) },${ Math.round( a.y * precision ) },${ Math.round( a.z * precision ) }`;
+			hashes[ 1 ] = `${ Math.round( b.x * precision ) },${ Math.round( b.y * precision ) },${ Math.round( b.z * precision ) }`;
+			hashes[ 2 ] = `${ Math.round( c.x * precision ) },${ Math.round( c.y * precision ) },${ Math.round( c.z * precision ) }`;
+
+			// skip degenerate triangles
+			if ( hashes[ 0 ] === hashes[ 1 ] || hashes[ 1 ] === hashes[ 2 ] || hashes[ 2 ] === hashes[ 0 ] ) {
 
 				continue;
 
 			}
 
-			intersectCount ++;
-			if ( intersectCount === 2 ) {
+			// iterate over every edge
+			for ( let j = 0; j < 3; j ++ ) {
 
-				break;
+				// get the first and next vertex making up the edge
+				const jNext = ( j + 1 ) % 3;
+				const vecHash0 = hashes[ j ];
+				const vecHash1 = hashes[ jNext ];
+				const v0 = _triangle[ vertKeys[ j ] ];
+				const v1 = _triangle[ vertKeys[ jNext ] ];
+
+				const hash = `${ vecHash0 }_${ vecHash1 }`;
+				const reverseHash = `${ vecHash1 }_${ vecHash0 }`;
+
+				if ( reverseHash in edgeData && edgeData[ reverseHash ] ) {
+
+					// if we found a sibling edge add it into the vertex array if
+					// it meets the angle threshold and delete the edge from the map.
+					const otherNormal = edgeData[ reverseHash ].normal;
+					const meetsThreshold = _normal.dot( otherNormal ) <= thresholdDot;
+					const projectionThreshold = Math.sign( projectionDir.dot( _normal ) ) !== Math.sign( projectionDir.dot( otherNormal ) );
+					if ( meetsThreshold || projectionThreshold ) {
+
+						const line = new Line3();
+						line.start.copy( v0 );
+						line.end.copy( v1 );
+						edges.push( line );
+
+					}
+
+					edgeData[ reverseHash ] = null;
+
+				} else if ( ! ( hash in edgeData ) ) {
+
+					// if we've already got an edge here then skip adding a new one
+					edgeData[ hash ] = {
+
+						index0: indexArr[ j ],
+						index1: indexArr[ jNext ],
+						normal: _normal.clone(),
+
+					};
+
+				}
 
 			}
 
 		}
 
-	}
+		// iterate over all remaining, unmatched edges and add them to the vertex array
+		for ( const key in edgeData ) {
 
-	if ( intersectCount === 2 ) {
+			if ( edgeData[ key ] ) {
 
-		// find the intersect line if any
-		_line0.delta( _dir0 ).normalize();
-		_line1.delta( _dir1 ).normalize();
+				const { index0, index1 } = edgeData[ key ];
+				_v0.fromBufferAttribute( positionAttr, index0 );
+				_v1.fromBufferAttribute( positionAttr, index1 );
 
-		// swap edges so they're facing in the same direction
-		if ( _dir0.dot( _dir1 ) < 0 ) {
+				const line = new Line3();
+				line.start.copy( _v0 );
+				line.end.copy( _v1 );
+				edges.push( line );
 
-			let tmp = _line1.start;
-			_line1.start = _line1.end;
-			_line1.end = tmp;
+			}
 
 		}
 
-		// check if the edges are overlapping
-		const s1 = _line0.start.dot( _dir0 );
-		const e1 = _line0.end.dot( _dir0 );
-		const s2 = _line1.start.dot( _dir0 );
-		const e2 = _line1.end.dot( _dir0 );
-		const separated1 = e1 < s2;
-		const separated2 = s1 < e2;
+		return edges;
 
-		if ( s1 !== e2 && s2 !== e1 && separated1 === separated2 ) {
+	};
+
+} )();
+
+// outputs the overlapping segment of a coplanar line and triangle
+export const getOverlappingLine = ( function () {
+
+	const _dir0 = new Vector3();
+	const _dir1 = new Vector3();
+	const _tempDir = new Vector3();
+	const _orthoPlane = new Plane();
+	const _line0 = new Line3();
+	const _line1 = new Line3();
+	const _tempLine = new Line3();
+
+	return function getOverlappingLine( line, triangle, lineTarget = new Line3() ) {
+
+		if ( triangle.needsUpdate ) {
+
+			triangle.needsUpdate();
+
+		}
+
+		// if the triangle is degenerate then return no overlap
+		if ( triangle.getArea() <= EPSILON ) {
 
 			return null;
 
 		}
 
-		// assign the target output
-		_tempDir.subVectors( _line0.start, _line1.start );
-		if ( _tempDir.dot( _dir0 ) > 0 ) {
+		const { points, plane } = triangle;
 
-			lineTarget.start.copy( _line0.start );
+		_line0.copy( line );
+		_line0.delta( _dir0 );
 
-		} else {
+		// if the line and triangle are not coplanar then return no overlap
+		const areCoplanar = plane.normal.dot( _dir0 ) === 0.0;
+		if ( ! areCoplanar ) {
 
-			lineTarget.start.copy( _line1.start );
-
-		}
-
-		_tempDir.subVectors( _line0.end, _line1.end );
-		if ( _tempDir.dot( _dir0 ) < 0 ) {
-
-			lineTarget.end.copy( _line0.end );
-
-		} else {
-
-			lineTarget.end.copy( _line1.end );
+			return null;
 
 		}
 
-		return lineTarget;
+		// a plane that's orthogonal to the triangle that the line lies on
+		_dir0.cross( plane.normal ).normalize();
+		_orthoPlane.setFromNormalAndCoplanarPoint( _dir0, _line0.start );
 
-	}
+		// find the line of intersection of the triangle along the plane if it exists
+		let intersectCount = 0;
+		for ( let i = 0; i < 3; i ++ ) {
 
-	return null;
+			const p1 = points[ i ];
+			const p2 = points[ ( i + 1 ) % 3 ];
 
-}
+			_tempLine.start.copy( p1 );
+			_tempLine.end.copy( p2 );
+			if ( _orthoPlane.distanceToPoint( _tempLine.end ) === 0 && _orthoPlane.distanceToPoint( _tempLine.start ) === 0 ) {
+
+				// if the edge lies on the plane then take the line
+				_line1.copy( _tempLine );
+				intersectCount = 2;
+				break;
+
+			} else if ( _orthoPlane.intersectLine( _tempLine, intersectCount === 0 ? _line1.start : _line1.end ) ) {
+
+				let p;
+				if ( intersectCount === 0 ) {
+
+					p = _line1.start;
+
+				} else {
+
+					p = _line1.end;
+
+				}
+
+				if ( p.distanceTo( p2 ) === 0.0 ) {
+
+					continue;
+
+				}
+
+				intersectCount ++;
+				if ( intersectCount === 2 ) {
+
+					break;
+
+				}
+
+			}
+
+		}
+
+		if ( intersectCount === 2 ) {
+
+			// find the intersect line if any
+			_line0.delta( _dir0 ).normalize();
+			_line1.delta( _dir1 ).normalize();
+
+			// swap edges so they're facing in the same direction
+			if ( _dir0.dot( _dir1 ) < 0 ) {
+
+				let tmp = _line1.start;
+				_line1.start = _line1.end;
+				_line1.end = tmp;
+
+			}
+
+			// check if the edges are overlapping
+			const s1 = _line0.start.dot( _dir0 );
+			const e1 = _line0.end.dot( _dir0 );
+			const s2 = _line1.start.dot( _dir0 );
+			const e2 = _line1.end.dot( _dir0 );
+			const separated1 = e1 < s2;
+			const separated2 = s1 < e2;
+
+			if ( s1 !== e2 && s2 !== e1 && separated1 === separated2 ) {
+
+				return null;
+
+			}
+
+			// assign the target output
+			_tempDir.subVectors( _line0.start, _line1.start );
+			if ( _tempDir.dot( _dir0 ) > 0 ) {
+
+				lineTarget.start.copy( _line0.start );
+
+			} else {
+
+				lineTarget.start.copy( _line1.start );
+
+			}
+
+			_tempDir.subVectors( _line0.end, _line1.end );
+			if ( _tempDir.dot( _dir0 ) < 0 ) {
+
+				lineTarget.end.copy( _line0.end );
+
+			} else {
+
+				lineTarget.end.copy( _line1.end );
+
+			}
+
+			return lineTarget;
+
+		}
+
+		return null;
+
+	};
+
+} )();
 
 // returns the the y value on the plane at the given point x, z
 export const getPlaneYAtPoint = ( function () {
@@ -296,21 +305,34 @@ export const getPlaneYAtPoint = ( function () {
 } )();
 
 // returns whether the given line is above the given triangle plane
-export function isLineAbovePlane( plane, line ) {
+export const isLineAbovePlane = ( function () {
 
-	_v0.lerpVectors( line.start, line.end, 0.5 );
-	getPlaneYAtPoint( plane, _v0, _v1 );
+	const _v0 = new Vector3();
+	const _v1 = new Vector3();
 
-	return _v1.y < _v0.y;
+	return function isLineAbovePlane( plane, line ) {
 
-}
+		_v0.lerpVectors( line.start, line.end, 0.5 );
+		getPlaneYAtPoint( plane, _v0, _v1 );
 
-export function isYProjectedLineDegenerate( line ) {
+		return _v1.y < _v0.y;
 
-	line.delta( _tempDir ).normalize();
-	return Math.abs( _tempDir.dot( _upVector ) ) >= 1.0 - EPSILON;
+	};
 
-}
+} )();
+
+export const isYProjectedLineDegenerate = ( function () {
+
+	const _tempDir = new Vector3();
+	const _upVector = new Vector3( 0, 1, 0 );
+	return function isYProjectedLineDegenerate( line ) {
+
+		line.delta( _tempDir ).normalize();
+		return Math.abs( _tempDir.dot( _upVector ) ) >= 1.0 - EPSILON;
+
+	};
+
+} )();
 
 // checks whether the y-projected triangle will be degerate
 export function isYProjectedTriangleDegenerate( tri ) {
@@ -362,6 +384,7 @@ export const getProjectedOverlaps = ( function () {
 	const _tempVec0 = new Vector3();
 	const _tempVec1 = new Vector3();
 	const _line = new Line3();
+	const _tri = new ExtendedTriangle();
 
 	return function getProjectedOverlaps( tri, line, overlapsTarget ) {
 
