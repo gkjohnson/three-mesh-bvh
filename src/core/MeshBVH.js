1c41f3b6cc7c1222d2cd7b62148856adb1516c9a
DM Liao <dm.liao@fb.com>
2022-08-17 21:31:53 -0700

    use attribute getters instead of direct array access in MeshBVH _traverse

--- src/core/MeshBVH.js
+++ src/core/MeshBVH.js
@@ -189,17 +189,7 @@ export class MeshBVH {
 		const geometry = this.geometry;
 		const indexArr = geometry.index.array;
 		const posAttr = geometry.attributes.position;
-		const posArr = posAttr.array;
-
-		// support for an interleaved position buffer
-		const bufferOffset = posAttr.offset || 0;
-		let stride = 3;
-		if ( posAttr.isInterleavedBufferAttribute ) {
-
-			stride = posAttr.data.stride;
-
-		}
-
+		
 		let buffer, uint32Array, uint16Array, float32Array;
 		let byteOffset = 0;
 		const roots = this._roots;
@@ -231,11 +221,10 @@ export class MeshBVH {
 				let maxy = - Infinity;
 				let maxz = - Infinity;
 				for ( let i = 3 * offset, l = 3 * ( offset + count ); i < l; i ++ ) {
-
-					const index = indexArr[ i ] * stride + bufferOffset;
-					const x = posArr[ index + 0 ];
-					const y = posArr[ index + 1 ];
-					const z = posArr[ index + 2 ];
+					const index = indexArr[ i ];
+					const x = posAttr.getX(index);
+					const y = posAttr.getY(index);
+					const z = posAttr.getZ(index);
 
 					if ( x < minx ) minx = x;
 					if ( x > maxx ) maxx = x;
