6475faa939483280437c25a8dfb1fe639ad6fa97
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-11-13 09:03:18 +0900

    Add "grid layers" parameter

--- example/utils/RenderSDFLayerMaterial.js
+++ example/utils/RenderSDFLayerMaterial.js
@@ -6,10 +6,17 @@ export class RenderSDFLayerMaterial extends ShaderMaterial {
 
 		super( {
 
+			defines: {
+
+				DISPLAY_GRID: 0,
+
+			},
+
 			uniforms: {
 
 				sdfTex: { value: null },
 				layer: { value: 0 },
+				layers: { value: 0 },
 
 			},
 
@@ -32,12 +39,29 @@ export class RenderSDFLayerMaterial extends ShaderMaterial {
 				varying vec2 vUv;
 				uniform sampler3D sdfTex;
 				uniform float layer;
+				uniform float layers;
 
 				void main() {
+
+					#if DISPLAY_GRID
+
+					float dim = ceil( sqrt( layers ) );
+					vec2 cell = floor( vUv * dim );
+					vec2 frac = vUv * dim - cell;
+					float zLayer = ( cell.y * dim + cell.x ) / ( dim * dim );
+
+					float dist = texture( sdfTex, vec3( frac, zLayer ) ).r;
+					gl_FragColor.rgb = dist > 0.0 ? vec3( 0, dist, 0 ) : vec3( - dist, 0, 0 );
+					gl_FragColor.a = 1.0;
+
+					#else
 
 					float dist = texture( sdfTex, vec3( vUv, layer ) ).r;
 					gl_FragColor.rgb = dist > 0.0 ? vec3( 0, dist, 0 ) : vec3( - dist, 0, 0 );
 					gl_FragColor.a = 1.0;
+
+					#endif
+
 
 				}
 			`
