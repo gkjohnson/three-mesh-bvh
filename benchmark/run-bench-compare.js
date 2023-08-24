ab39200d7df94d6b315790c7d1208702545581e4
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-08-24 23:52:31 +0900

    add bench compare

---
+++ benchmark/run-bench-compare.js
@@ -0,0 +1,44 @@
+import simpleGit from 'simple-git';
+import { exec } from 'child_process';
+
+( async() => {
+
+	const git = simpleGit();
+	const status = await git.status();
+
+	const modified = status.modified.length + status.created.length + status.renamed.length + status.deleted.length;
+	if ( modified !== 0 ) {
+
+		console.error( 'Current branch is not clean' );
+		process.exit( 1 );
+
+	}
+
+	const currentBranch = status.current;
+	// await runScript( 'node ./benchmark/run-benchmark.js --long --json > pr-benchmark.json' );
+	// await git.checkout( 'master' );
+	// await runScript( 'node ./benchmark/run-benchmark.js --long --json > master-benchmark.json' );
+
+	await runScript( 'node ./benchmark/compare-bench.js' );
+
+	await git.checkout( currentBranch );
+
+} )();
+
+function runScript( command ) {
+
+	return new Promise( ( resolve, reject ) => {
+
+		const proc = exec( command );
+		proc.stderr.pipe( process.stderr );
+		proc.stdout.pipe( process.stdout );
+		proc.on( 'exit', code => {
+
+			if ( code === 0 ) resolve();
+			else reject();
+
+		} );
+
+	} );
+
+}
