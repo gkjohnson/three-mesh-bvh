6958cedc4cc048d8cc5383f6b016fdcc5cd9e53a
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-08-25 12:50:13 +0900

    remove liens

--- src/core/cast/raycastFirst.template.js
+++ src/core/cast/raycastFirst.template.js
@@ -4,7 +4,6 @@ import { BufferStack } from '../utils/BufferStack.js';
 import { intersectRay } from '../utils/intersectUtils.js';
 import { intersectClosestTri } from '../utils/iterationUtils.generated.js';
 import { intersectClosestTri_indirect } from '../utils/iterationUtils_indirect.generated.js';
-
 const _boxIntersection = /* @__PURE__ */ new Vector3();
 const _xyzFields = [ 'x', 'y', 'z' ];
 export function raycastFirst/* @echo INDIRECT_STRING */( bvh, root, side, ray ) {
