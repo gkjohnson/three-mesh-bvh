f557cc36917af8a4924dfc43b15eee4aa87f4913
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-08-25 16:45:07 +0900

    Remove build steps

--- benchmark/run-bench-compare.js
+++ benchmark/run-bench-compare.js
@@ -17,12 +17,12 @@ import { exec } from 'child_process';
 	const currentBranch = status.current;
 
 	console.log( 'Running Benchmark' );
-	await runScript( 'npm run build-silent' );
+	// await runScript( 'npm run build-silent' );
 	await runScript( 'node ./benchmark/run-benchmark.js --long --json > pr-benchmark.json' );
 
 	console.log( 'Running Master Benchmark' );
 	await git.checkout( 'master' );
-	await runScript( 'npm run build-silent' );
+	// await runScript( 'npm run build-silent' );
 	await runScript( 'node ./benchmark/run-benchmark.js --long --json > master-benchmark.json' );
 
 	console.log( 'Comparing Benchmarks' );
