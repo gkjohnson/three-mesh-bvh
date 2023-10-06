6370c1d18f274076a6e14027e3597f77bcc1b3be
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-10-06 19:30:20 +0900

    Remove memory allocations for boxes

--- src/core/cast/bvhcast_new.js
+++ src/core/cast/bvhcast_new.js
@@ -2,9 +2,11 @@ import { Box3, Matrix4 } from 'three';
 import { BufferStack } from '../utils/BufferStack.js';
 import { BOUNDING_DATA_INDEX, COUNT, IS_LEAF, LEFT_NODE, OFFSET, RIGHT_NODE } from '../utils/nodeBufferUtils.js';
 import { arrayToBox } from '../../utils/ArrayBoxUtilities.js';
+import { PrimitivePool } from '../../utils/PrimitivePool.js';
 
 const _bufferStack1 = new BufferStack.constructor();
 const _bufferStack2 = new BufferStack.constructor();
+const _boxPool = new PrimitivePool( () => new Box3() );
 
 const _lbox2 = new Box3();
 const _rbox2 = new Box3();
@@ -93,11 +95,12 @@ function _traverse(
 
 	const bufferStack1 = reversed ? _bufferStack2 : _bufferStack1;
 	const bufferStack2 = reversed ? _bufferStack1 : _bufferStack2;
-	if ( currBox === null ) {
+	let localBox = currBox;
+	if ( localBox === null ) {
 
-		currBox = new Box3();
-		arrayToBox( BOUNDING_DATA_INDEX( node1Index32 ), bufferStack1.float32Array, currBox );
-		currBox.applyMatrix4( matrix1to2 );
+		localBox = _boxPool.getPrimitive();
+		arrayToBox( BOUNDING_DATA_INDEX( node1Index32 ), bufferStack1.float32Array, localBox );
+		localBox.applyMatrix4( matrix1to2 );
 
 	}
 
@@ -105,6 +108,7 @@ function _traverse(
 	const node2Index16 = node2Index32 * 2;
 	const isLeaf1 = IS_LEAF( node1Index16, bufferStack1.uint16Array );
 	const isLeaf2 = IS_LEAF( node2Index16, bufferStack2.uint16Array );
+	let result = false;
 	if ( isLeaf2 && isLeaf1 ) {
 
 		// if both bounds are leaf nodes then fire the callback if the boxes intersect
@@ -118,7 +122,7 @@ function _traverse(
 
 		if ( reversed ) {
 
-			return intersectsRangesFunc(
+			result = intersectsRangesFunc(
 				OFFSET( node2Index32, bufferStack2.uint32Array ), COUNT( node2Index32 * 2, bufferStack2.uint16Array ),
 				OFFSET( node1Index32, bufferStack1.uint32Array ), COUNT( node1Index32 * 2, bufferStack1.uint16Array ),
 				depth2, node2IndexByteOffset + node2Index32,
@@ -127,7 +131,7 @@ function _traverse(
 
 		} else {
 
-			return intersectsRangesFunc(
+			result = intersectsRangesFunc(
 				OFFSET( node1Index32, bufferStack1.uint32Array ), COUNT( node1Index32 * 2, bufferStack1.uint16Array ),
 				OFFSET( node2Index32, bufferStack2.uint32Array ), COUNT( node2Index32 * 2, bufferStack2.uint16Array ),
 				depth1, node1IndexByteOffset + node1Index32,
@@ -148,21 +152,23 @@ function _traverse(
 
 		// }
 
-		currBox = new Box3();
-		arrayToBox( BOUNDING_DATA_INDEX( node2Index32 ), bufferStack2.float32Array, currBox );
-		currBox.applyMatrix4( matrix2to1 );
+		const newBox = _boxPool.getPrimitive();
+		arrayToBox( BOUNDING_DATA_INDEX( node2Index32 ), bufferStack2.float32Array, newBox );
+		newBox.applyMatrix4( matrix2to1 );
 
 		const cl1 = LEFT_NODE( node1Index32 );
 		const cr1 = RIGHT_NODE( node1Index32, bufferStack1.uint32Array );
-		return _traverse(
+		result = _traverse(
 			node2Index32, cl1, matrix1to2, matrix2to1, intersectsRangesFunc,
 			node2IndexByteOffset, node1IndexByteOffset, depth2, depth1 + 1,
-			currBox, ! reversed,
+			newBox, ! reversed,
 		) || _traverse(
 			node2Index32, cr1, matrix1to2, matrix2to1, intersectsRangesFunc,
 			node2IndexByteOffset, node1IndexByteOffset, depth2, depth1 + 1,
-			currBox, ! reversed,
+			newBox, ! reversed,
 		);
+
+		_boxPool.releasePrimitive( newBox );
 
 	} else {
 
@@ -171,46 +177,48 @@ function _traverse(
 		arrayToBox( BOUNDING_DATA_INDEX( cl2 ), bufferStack2.float32Array, _lbox2 );
 		arrayToBox( BOUNDING_DATA_INDEX( cr2 ), bufferStack2.float32Array, _rbox2 );
 
-		const leftIntersects = currBox.intersectsBox( _lbox2 );
-		const rightIntersects = currBox.intersectsBox( _rbox2 );
+		const leftIntersects = localBox.intersectsBox( _lbox2 );
+		const rightIntersects = localBox.intersectsBox( _rbox2 );
 		if ( leftIntersects && rightIntersects ) {
 
-			return _traverse(
+			result = _traverse(
 				node1Index32, cl2, matrix2to1, matrix1to2, intersectsRangesFunc,
 				node1IndexByteOffset, node2IndexByteOffset, depth1, depth2 + 1,
-				currBox, reversed,
+				localBox, reversed,
 			) || _traverse(
 				node1Index32, cr2, matrix2to1, matrix1to2, intersectsRangesFunc,
 				node1IndexByteOffset, node2IndexByteOffset, depth1, depth2 + 1,
-				currBox, reversed,
+				localBox, reversed,
 			);
 
 		} else if ( leftIntersects ) {
 
 			if ( isLeaf1 ) {
 
-				return _traverse(
+				result = _traverse(
 					node1Index32, cl2, matrix2to1, matrix1to2, intersectsRangesFunc,
 					node1IndexByteOffset, node2IndexByteOffset, depth1, depth2 + 1,
-					currBox, reversed,
+					localBox, reversed,
 				);
 
 			} else {
 
-				currBox = new Box3();
-				currBox.copy( _lbox2 ).applyMatrix4( matrix2to1 );
+				const newBox = _boxPool.getPrimitive();
+				newBox.copy( _lbox2 ).applyMatrix4( matrix2to1 );
 
 				const cl1 = LEFT_NODE( node1Index32 );
 				const cr1 = RIGHT_NODE( node1Index32, bufferStack1.uint32Array );
-				return _traverse(
+				result = _traverse(
 					cl2, cl1, matrix1to2, matrix2to1, intersectsRangesFunc,
 					node2IndexByteOffset, node1IndexByteOffset, depth2, depth1 + 1,
-					currBox, ! reversed,
+					newBox, ! reversed,
 				) || _traverse(
 					cl2, cr1, matrix1to2, matrix2to1, intersectsRangesFunc,
 					node2IndexByteOffset, node1IndexByteOffset, depth2, depth1 + 1,
-					currBox, ! reversed,
+					newBox, ! reversed,
 				);
+
+				_boxPool.releasePrimitive( newBox );
 
 			}
 
@@ -218,38 +226,44 @@ function _traverse(
 
 			if ( isLeaf1 ) {
 
-				return _traverse(
+				result = _traverse(
 					node1Index32, cr2, matrix2to1, matrix1to2, intersectsRangesFunc,
 					node1IndexByteOffset, node2IndexByteOffset, depth1, depth2 + 1,
-					currBox, reversed,
+					localBox, reversed,
 				);
 
 			} else {
 
-				currBox = new Box3();
-				currBox.copy( _rbox2 ).applyMatrix4( matrix2to1 );
+				const newBox = _boxPool.getPrimitive();
+				newBox.copy( _rbox2 ).applyMatrix4( matrix2to1 );
 
 				const cl1 = LEFT_NODE( node1Index32 );
 				const cr1 = RIGHT_NODE( node1Index32, bufferStack1.uint32Array );
-				return _traverse(
+				result = _traverse(
 					cr2, cl1, matrix1to2, matrix2to1, intersectsRangesFunc,
 					node2IndexByteOffset, node1IndexByteOffset, depth2, depth1 + 1,
-					currBox, ! reversed,
+					newBox, ! reversed,
 				) || _traverse(
 					cr2, cr1, matrix1to2, matrix2to1, intersectsRangesFunc,
 					node2IndexByteOffset, node1IndexByteOffset, depth2, depth1 + 1,
-					currBox, ! reversed,
+					newBox, ! reversed,
 				);
 
+				_boxPool.releasePrimitive( newBox );
+
 			}
-
-		} else {
-
-			return false;
 
 		}
 
 	}
+
+	if ( currBox === null ) {
+
+		_boxPool.releasePrimitive( localBox );
+
+	}
+
+	return result;
 
 }
 
