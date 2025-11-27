35f46728f00a162d967a13bc50ebdb91216c8a7e
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-11-27 18:23:45 +0900

    Fix tests

--- vitest.config.js
+++ vitest.config.js
@@ -4,19 +4,5 @@ export default defineConfig( {
 	test: {
 		environment: 'jsdom',
 		globals: true,
-		pool: 'forks',
-		poolOptions: {
-			forks: {
-				singleFork: false,
-			},
-		},
-	},
-	worker: {
-		format: 'es',
-	},
-	server: {
-		fs: {
-			allow: [ '.' ],
-		},
 	},
 } );
