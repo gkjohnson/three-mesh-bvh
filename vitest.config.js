05d9d1d7c3679d24760c0e6e6fa04fcd572c0b02
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-12-04 10:48:12 +0900

    Cleanup

--- vitest.config.js
+++ vitest.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig( {
 	test: {
-		environment: 'jsdom',
+		environment: 'node',
 		globals: true,
-		setupFiles: [ '@vitest/web-worker', './test/matchers/toEqualBVH.js' ],
+		setupFiles: [ './test/matchers/toEqualBVH.js' ],
 	},
 } );
