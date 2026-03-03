0796dd4a23cb39e2e990dbddbdf8215c2bcc7176
gkjohnson <gkjohnson@users.noreply.github.com>
2026-03-03 13:07:42 +0000

    update builds

---
+++ example/bundle/assets/BVHRaycastUtils-BM5XEqDY.js
@@ -0,0 +1,2 @@
+import{aB as t}from"./ExtendedTriangle-B5A8oPHF.js";const a=t.prototype.raycast,o=function(s,e){this.boundsTree?this.boundsTree.raycastObject3D(this,s,e):a.call(this,s,e)};function n(s,e){if(this.boundsTree)return this.boundsTree.raycast(s,e),!1}export{n as o,o as s};
+//# sourceMappingURL=BVHRaycastUtils-BM5XEqDY.js.map
