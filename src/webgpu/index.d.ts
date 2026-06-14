e16442b41a7d83e30b53f82436ff4fd3a00da945
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-06-14 23:14:55 +0900

    Update types

--- src/webgpu/index.d.ts
+++ src/webgpu/index.d.ts
@@ -1,31 +1,7 @@
-import { wgsl, wgslFn } from 'three/tsl';
-import { ObjectBVH } from '../core/ObjectBVH.js';
-
-export const bvhIntersectFirstHit: ReturnType<typeof wgslFn>;
-export const closestPointToPoint: ReturnType<typeof wgslFn>;
-
-export const bvhNodeBoundsStruct: ReturnType<typeof wgsl>;
-export const bvhNodeStruct: ReturnType<typeof wgsl>;
-export const rayStruct: ReturnType<typeof wgsl>;
-export const intersectionResultStruct: ReturnType<typeof wgsl>;
-export const closestPointToPointResultStruct: ReturnType<typeof wgsl>;
-export const closestPointToTriangleResultStruct: ReturnType<typeof wgsl>;
-
-export const intersectsTriangle: ReturnType<typeof wgslFn>;
-export const intersectRayTriangle: ReturnType<typeof wgslFn>;
-export const pointQueryResultStruct: unknown;
-export const intersectTriangles: ReturnType<typeof wgslFn>;
-export const intersectsBounds: ReturnType<typeof wgslFn>;
-export const getVertexAttribute: ReturnType<typeof wgslFn>;
-export const ndcToCameraRay: ReturnType<typeof wgslFn>;
-export const closestPointToTriangle: ReturnType<typeof wgslFn>;
-export const distanceToTriangles: ReturnType<typeof wgslFn>;
-export const distanceSqToBounds: ReturnType<typeof wgslFn>;
-export const distanceSqToBVHNodeBoundsPoint: ReturnType<typeof wgslFn>;
+import { ObjectBVH } from 'three-mesh-bvh';
 
 export class BVHComputeData {
 
-	bvh: ObjectBVH;
 	autogenerateBvh: boolean;
 	attributes: Record<string, string>;
 
@@ -63,10 +39,5 @@ export class BVHComputeData {
 		transformShapeFn?: unknown;
 		transformResultFn?: unknown;
 	} ): unknown;
-
-	writeTransformData( info: object, premultiplyMatrix: unknown, writeOffset: number, targetBuffer: ArrayBuffer ): void;
-	getBVH( object: object, instanceId: number, rangeTarget: object ): unknown;
-	getDefaultAttributeValue( key: string, target: unknown ): unknown;
-	dispose(): void;
 
 }
