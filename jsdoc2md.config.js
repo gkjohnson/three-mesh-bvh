df1ced68d95861b4c30ccf15d4f66d4e36bff5db
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-06-16 17:02:14 +0900

    Update jsdoc2md config

---
+++ jsdoc2md.config.js
@@ -0,0 +1,13 @@
+export default [
+	{
+		output: './API.md',
+		title: 'three-mesh-bvh',
+		source: './src',
+		exclude: './src/webgpu',
+	},
+	{
+		output: './WEBGPU_API.md',
+		title: 'three-mesh-bvh/webgpu',
+		source: './src/webgpu',
+	},
+];
